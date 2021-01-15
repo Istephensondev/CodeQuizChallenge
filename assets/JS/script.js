01fc52a6bd039f60341bc8688b4ae49832c62567
Ivy Stephenson <istephensondev@gmail.com>
2021-01-15 13:18:58 -0600

    create timer

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,2 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var buttonEl = document.querySelector("#start");
 console.log(buttonEl);
+
+buttonEl.addEventListener("click", function () {
+    var time = 90;
+
+    setInterval(function () {
+        time = time - 1;
+        console.log(time);
+        document.getElementById('timer').innerText = time;
+    }, 1000);
+});
+
+  // start timer 
+
